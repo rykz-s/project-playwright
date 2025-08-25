6778c37f75f139011eb6c70a42a21349543e5647
unknown <ryukazuas@gmail.com>
2025-08-25 07:54:32 +0700

    adding 1 positive test case  and 1 negative test case

--- src/features/contact_us.feature
+++ src/features/contact_us.feature
@@ -9,4 +9,15 @@ Feature: WebdriverUniversity.com - Contact Us Page
         And I type a comment
         And I click on the submit button
         Then I should be presented with a succesful contact us submission message
+
+    Scenario: Invalid Contact Us Form Submission
+        Given I navigate to the webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to the new browser tab
+        And I type a first name
+        And I type a last name
+        # And I enter an email address
+        And I type a comment
+        And I click on the submit button
+        Then I should be presented with a unsuccesful contact message    
         
--- src/step-definitions/contactUs_steps.ts
+++ src/step-definitions/contactUs_steps.ts
@@ -1,8 +1,55 @@
-import {AfterAll, Given, When} from "@cucumber/cucumber";
+import { AfterAll, Given, When, Then } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
+import { expect } from '@playwright/test';
 
 When('I type a first name', async () => {
     const firstName_field = pageFixture.page.getByRole('textbox', { name: 'First Name' });
     await firstName_field.fill("Ryu");
     // await pageFixture.page.pause();
 });
+
+
+When('I type a last name', async () => {
+    await pageFixture.page.getByRole('textbox', { name: 'Last Name' }).fill("Andara")
+});
+
+
+When('I enter an email address', async () => {
+    await pageFixture.page.getByRole('textbox', { name: 'Email Address' }).fill("ryukazuas@gmail.com")
+});
+
+When('I type a comment', async () => {
+    await pageFixture.page.getByRole('textbox', { name: 'Comments' }).fill("The Comment");
+});
+
+When('I click on the submit button', async () => {
+    // wait button until loaded
+    await pageFixture.page.waitForSelector('input[value="SUBMIT"]');
+    // once loaded, click on the button
+    await pageFixture.page.click('input[value="SUBMIT"]');
+});
+
+Then('I should be presented with a succesful contact us submission message', async () => {
+    // waiting for the header text element
+    await pageFixture.page.waitForSelector('#contact_reply h1', {timeout: 60000});
+
+    // get the text from the h1 element
+    const text = await pageFixture.page.innerText('#contact_reply h1');
+    const expectedText = "Thank You for your Message!";
+    // use playwright's "Expect" function to assert the text of the h1 element
+    // await pageFixture.page.pause();
+    expect(text).toBe(expectedText);
+});
+
+Then('I should be presented with a unsuccesful contact message', async() => {
+    // Wait for the <body> element
+    await pageFixture.page.waitForSelector("body");
+
+    // Locate the <body> element
+    const bodyElement = await pageFixture.page.locator("body");
+
+    // Extract the text from body element
+    const bodyText = await bodyElement.textContent();
+    await expect(bodyText).toMatch(/Error: (all fields are required|Invalid email address)/);
+
+})
