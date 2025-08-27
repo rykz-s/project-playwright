e9d125271b1ef2beb8a9cf19a06efbf7a109c89d
unknown <ryukazuas@gmail.com>
2025-08-27 07:57:49 +0700

    adding valid submission with random data using faker.js

--- src/features/contact_us.feature
+++ src/features/contact_us.feature
@@ -30,5 +30,15 @@ Feature: WebdriverUniversity.com - Contact Us Page
         And I enter a specific email address "ryukazuas@gmail.com"
         And I type a specific text "Hello World!" and a number 2 within the comment input field
         And I click on the submit button
-        Then I should be presented with a succesful contact us submission message    
-        
+        Then I should be presented with a succesful contact us submission message
+
+    Scenario: Valid Contact Us Form Submission - Using Random Data
+        Given I navigate to the webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to the new browser tab
+        And I type a random first name
+        And I type a random last name
+        And I enter a random email address
+        And I type a comment
+        And I click on the submit button
+        Then I should be presented with a succesful contact us submission message
--- src/step-definitions/contactUs_steps.ts
+++ src/step-definitions/contactUs_steps.ts
@@ -1,6 +1,7 @@
 import { AfterAll, Given, When, Then } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
 import { expect } from '@playwright/test';
+import { faker } from "@faker-js/faker";
 
 When('I type a first name', async () => {
     const firstName_field = pageFixture.page.getByRole('textbox', { name: 'First Name' });
@@ -31,7 +32,7 @@ When('I click on the submit button', async () => {
 
 Then('I should be presented with a succesful contact us submission message', async () => {
     // waiting for the header text element
-    await pageFixture.page.waitForSelector('#contact_reply h1', {timeout: 60000});
+    await pageFixture.page.waitForSelector('#contact_reply h1', { timeout: 60000 });
 
     // get the text from the h1 element
     const text = await pageFixture.page.innerText('#contact_reply h1');
@@ -41,7 +42,7 @@ Then('I should be presented with a succesful contact us submission message', asy
     expect(text).toBe(expectedText);
 });
 
-Then('I should be presented with a unsuccesful contact message', async() => {
+Then('I should be presented with a unsuccesful contact message', async () => {
     // Wait for the <body> element
     await pageFixture.page.waitForSelector("body");
 
@@ -55,25 +56,48 @@ Then('I should be presented with a unsuccesful contact message', async() => {
 
 // Cucumber Exppresion:
 
-    When('I type a specific first name {string}', async(firstName: string) => {
-        await pageFixture.page.getByRole('textbox', { name: 'First Name' })
-            .fill(firstName);
+When('I type a specific first name {string}', async (firstName: string) => {
+    await pageFixture.page.getByRole('textbox', { name: 'First Name' })
+        .fill(firstName);
 
-    });
+});
 
-    When('I type a specific last name {string}', async(lastName: string) => {
-        await pageFixture.page.getByRole('textbox', { name: 'Last Name' })
-            .fill(lastName);
-    });
+When('I type a specific last name {string}', async (lastName: string) => {
+    await pageFixture.page.getByRole('textbox', { name: 'Last Name' })
+        .fill(lastName);
+});
 
 
-    When('I enter a specific email address {string}', async(emailAddress: string) => {
-        await pageFixture.page.getByRole('textbox', { name: 'Email Address' })
-            .fill(emailAddress);
-    });
+When('I enter a specific email address {string}', async (emailAddress: string) => {
+    await pageFixture.page.getByRole('textbox', { name: 'Email Address' })
+        .fill(emailAddress);
+});
 
-    When('I type a specific text {string} and a number {int} within the comment input field', async(word: string, number: number) => {
-        await pageFixture.page.getByRole('textbox', { name: 'Comments' })
-            .fill(word + " " + number);
-        // await pageFixture.page.pause();
-    });
+When('I type a specific text {string} and a number {int} within the comment input field', async (word: string, number: number) => {
+    await pageFixture.page.getByRole('textbox', { name: 'Comments' })
+        .fill(word + " " + number);
+    // await pageFixture.page.pause();
+});
+
+// Random Data using Faker.js
+When('I type a random first name', async () => {
+    // Faker code
+    const randomFirstName = faker.person.firstName();
+    await pageFixture.page.getByRole('textbox', { name: 'First Name' })
+        .fill(randomFirstName);
+});
+
+When('I type a random last name', async () => {
+    // Faker code
+    const randomLastName = faker.person.lastName();
+    await pageFixture.page.getByRole('textbox', { name: 'Last Name' })
+        .fill(randomLastName);
+});
+
+When('I enter a random email address', async () => {
+    // Faker code
+    const randomEmail = faker.internet.email();
+    await pageFixture.page.getByRole('textbox', { name: 'Email Address' })
+        .fill(randomEmail);
+    await pageFixture.page.pause();
+});
